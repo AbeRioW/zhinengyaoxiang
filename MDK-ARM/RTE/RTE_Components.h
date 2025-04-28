7993ef5a4faa947dfaa8db5e0c450f1d6ca44359
Rio <yp.wang@jingda.techgit config --global uaer.name riogit config --global user.email yp.wang@jingda.tech>
2025-04-28 13:22:29 +0800

    project done

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'yaoxiang' 
+ * Target:  'yaoxiang' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
